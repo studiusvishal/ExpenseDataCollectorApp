14018da22b9d3ceca8951c3f445169d519244d43
Vishal Bhavsar <vishu.bhavsar@gmail.com>
2021-08-23 19:23:14 -0700

    Add floating action button with navigation bar

--- app/src/main/java/com/bhavsar/vishal/app/expensedatacollector/activities/MainActivity.java
+++ app/src/main/java/com/bhavsar/vishal/app/expensedatacollector/activities/MainActivity.java
@@ -3,10 +3,12 @@ package com.bhavsar.vishal.app.expensedatacollector.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.bhavsar.vishal.app.expensedatacollector.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +16,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button addExpenseButton = findViewById(R.id.buttonAddExpense);
+        addExpenseButton.setOnClickListener(this::onClickAddNewExpense);
+
+        final BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setBackground(null);
+        bottomNavigationView.getMenu().getItem(2).setEnabled(false);
     }
 
     public void onClickAddNewExpense(final View view) {
